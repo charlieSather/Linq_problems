3cd03814b9c7601751d3fe9a8b394904666fa5d0
Charlie Sather <charliesather18@gmail.com>
2020-02-10 07:28:51 -0600

    Solved problem set

--- LinqProblems/Linq_Problems.cs
+++ LinqProblems/Linq_Problems.cs
@@ -7,6 +7,27 @@ using System.Threading.Tasks;
 namespace LinqProblems
 {
     class Linq_Problems
-    {       
+    {
+        public List<string> ProblemOne(List<string> strings)
+        {
+            return strings.Where(x => x.Contains("th")).ToList();
+        }
+
+        public List<string> ProblemTwo(List<string> strings)
+        {
+            return strings.Distinct().ToList();
+        }
+
+        public double ProblemThree(List<string> grades)
+        {
+            return grades.Select(x => x.Split(',')).Select(x => x.Select(y => double.Parse(y)).OrderBy(z => z).Skip(1)).SelectMany(x => x).Average();
+        }
+
+        public string ProblemFour(string letters)
+        {
+            string LETTERS = letters.ToUpper();
+            //return new string(letters.ToUpper().OrderBy(c => c).Distinct().Select(x => $"{x}{letters.ToUpper().Count(y => y == x)}").SelectMany(x => x.ToCharArray()).ToArray());  
+            return string.Join("", LETTERS.OrderBy(c => c).Distinct().Select(x => $"{x}{LETTERS.Count(y => y == x)}"));  
+        }
     }
 }
--- LinqProblems/Program.cs
+++ LinqProblems/Program.cs
@@ -10,7 +10,26 @@ namespace LinqProblems
     {
         static void Main(string[] args)
         {
-           
+            Linq_Problems lq = new Linq_Problems();
+
+            var result = lq.ProblemOne(new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" });                     
+            result.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine();
+
+            result = lq.ProblemTwo(new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" });
+            result.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine();
+
+
+            var result2 = lq.ProblemThree(new List<string>() { "80,100,92,89,65", "93,81,78,84,69", "73,88,83,99,64", "98,100,66,74,55" });
+            Console.WriteLine(result2);
+            Console.WriteLine();
+
+
+            Console.WriteLine(lq.ProblemFour("Terrill"));
+
+            Console.ReadLine();
+
         }
     }
 }
